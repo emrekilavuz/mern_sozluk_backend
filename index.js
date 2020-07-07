c803fd9a2af1829760feea0cc99731013d3fff10
unknown <emrekilavuz@hotmail.com>
2020-07-07 17:21:01 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({path : './config/config.env'});
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hello postman");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT} in mode ${process.env.NODE_ENV}`);
+});
+
